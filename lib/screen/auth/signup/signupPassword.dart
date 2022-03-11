4f6819bb12e4581871bb5eb54c9c9b31cee6f076
Shatha928 <439200928@student.ksu.edu.sa>
2022-03-11 20:35:34 +0300

    pass the email to next pages

--- lib/screen/auth/signup/signupPassword.dart
+++ lib/screen/auth/signup/signupPassword.dart
@@ -1,13 +1,8 @@
-import 'dart:io';
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 /*extra */
-import 'package:flutter/services.dart';
-import 'package:flutter/gestures.dart';
 
 /*pages */
-import 'package:gp1_7_2022/screen/auth/signup_login.dart';
-import 'package:gp1_7_2022/screen/auth/signup/signup.dart';
+
 /*colors */
 import 'package:gp1_7_2022/config/palette.dart';
 
@@ -33,229 +28,227 @@ class _signupPasswordState extends State<signupPassword> {
       ),
       //fix overloade error
       resizeToAvoidBottomInset: false,
-      body: Container(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
+      body: Column(
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
 
-            /*first column*/
-            Expanded(
-              child: Container(
-                margin:  EdgeInsets.symmetric(horizontal: 40),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
+          /*first column*/
+          Expanded(
+            child: Container(
+              margin:  const EdgeInsets.symmetric(horizontal: 40),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
 
-                    /*Enter your email*/
-                    Container(
-                      padding: EdgeInsets.symmetric(vertical: 10),
-                      // color: Colors.red,
-                      child:Center(
-                        child: Text(
-                          "Create a password",
-                          textAlign: TextAlign.center,
-                          style: TextStyle(
-                            fontSize: 30,
-                            color: Palette.textColor,
-                          ),
+                  /*Enter your email*/
+                  Container(
+                    padding: const EdgeInsets.symmetric(vertical: 10),
+                    // color: Colors.red,
+                    child:const Center(
+                      child: Text(
+                        "Create a password",
+                        textAlign: TextAlign.center,
+                        style: TextStyle(
+                          fontSize: 30,
+                          color: Palette.textColor,
                         ),
                       ),
                     ),
+                  ),
 
-                    Container(
-                      padding: EdgeInsets.symmetric( vertical: 10),
-                      child: Center(
-                        child:  Text(
-                          'You need to enter your password twice, to mack sure you enter it right.',
-                          style: new TextStyle(
-                            fontSize: 18,
-                            color: Palette.grey,
-                          ),
-                          textAlign: TextAlign.center,
+                  Container(
+                    padding: const EdgeInsets.symmetric( vertical: 10),
+                    child: const Center(
+                      child:  Text(
+                        'You need to enter your password twice, to mack sure you enter it right.',
+                        style: TextStyle(
+                          fontSize: 18,
+                          color: Palette.grey,
                         ),
+                        textAlign: TextAlign.center,
                       ),
                     ),
+                  ),
 
 
 
-                    /*form*/
-                    Form(
-                      child: Column(
-                          children:[ Column(
-                            children: [
+                  /*form*/
+                  Form(
+                    child: Column(
+                        children:[ Column(
+                          children: [
 
-                              /*passwrd*/
-                              Container(
-                                margin: EdgeInsets.symmetric(vertical: 10),
-                                child: TextFormField(
-                                  //design
-                                  decoration: InputDecoration(
+                            /*password*/
+                            Container(
+                              margin: const EdgeInsets.symmetric(vertical: 10),
+                              child: TextFormField(
+                                //design
+                                decoration: const InputDecoration(
 
-                                    /*background color*/
-                                    fillColor: Palette.lightgrey,
-                                    filled: true,
+                                  /*background color*/
+                                  fillColor: Palette.lightgrey,
+                                  filled: true,
 
-                                    /*hint*/
-                                    border: OutlineInputBorder(),
-                                    hintText: "Password",
-                                    hintStyle: TextStyle(
-                                        fontSize: 18.0,
-                                        color: Palette.grey
-                                    ),
-
-                                    /*Border*/
-                                    focusedBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Palette.midgrey,
-                                      ),
-                                    ),
-                                    enabledBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Palette.midgrey,
-                                        width: 2.0,
-                                      ),
-                                    ),
+                                  /*hint*/
+                                  border: OutlineInputBorder(),
+                                  hintText: "Password",
+                                  hintStyle: TextStyle(
+                                      fontSize: 18.0,
+                                      color: Palette.grey
                                   ),
 
-                                  obscureText: true,
-                                  //function
-                                  onChanged: (val){
-
-                                  },
-                                ),
-                              ),
-                              /*end of password*/
-
-                              /*passwrd*/
-                              Container(
-                                margin: EdgeInsets.symmetric(vertical: 10),
-                                child: TextFormField(
-                                  //design
-                                  decoration: InputDecoration(
-
-                                    /*background color*/
-                                    fillColor: Palette.lightgrey,
-                                    filled: true,
-
-                                    /*hint*/
-                                    border: OutlineInputBorder(),
-                                    hintText: "Confirm password",
-                                    hintStyle: TextStyle(
-                                        fontSize: 18.0,
-                                        color: Palette.grey
-                                    ),
-
-                                    /*Border*/
-                                    focusedBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Palette.midgrey,
-                                      ),
-                                    ),
-                                    enabledBorder: OutlineInputBorder(
-                                      borderSide: BorderSide(
-                                        color: Palette.midgrey,
-                                        width: 2.0,
-                                      ),
+                                  /*Border*/
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Palette.midgrey,
                                     ),
                                   ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Palette.midgrey,
+                                      width: 2.0,
+                                    ),
+                                  ),
+                                ),
 
-                                  obscureText: true,
-                                  //function
-                                  onChanged: (val){
+                                obscureText: true,
+                                //function
+                                onChanged: (val){
 
-                                  },
+                                },
+                              ),
+                            ),
+                            /*end of password*/
+
+                            /*password*/
+                            Container(
+                              margin: const EdgeInsets.symmetric(vertical: 10),
+                              child: TextFormField(
+                                //design
+                                decoration: const InputDecoration(
+
+                                  /*background color*/
+                                  fillColor: Palette.lightgrey,
+                                  filled: true,
+
+                                  /*hint*/
+                                  border: OutlineInputBorder(),
+                                  hintText: "Confirm password",
+                                  hintStyle: TextStyle(
+                                      fontSize: 18.0,
+                                      color: Palette.grey
+                                  ),
+
+                                  /*Border*/
+                                  focusedBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Palette.midgrey,
+                                    ),
+                                  ),
+                                  enabledBorder: OutlineInputBorder(
+                                    borderSide: BorderSide(
+                                      color: Palette.midgrey,
+                                      width: 2.0,
+                                    ),
+                                  ),
+                                ),
+
+                                obscureText: true,
+                                //function
+                                onChanged: (val){
+
+                                },
+                              ),
+                            ),
+                            /*end of password*/
+
+
+                            /*next button*/
+                            Container(
+                              margin: const EdgeInsets.symmetric(vertical: 10),
+                              alignment: Alignment.center,
+                              width: double.infinity,
+                              height: 50.0,
+                              /*button colors*/
+                              decoration: const BoxDecoration(
+                                borderRadius: BorderRadius.all(Radius.circular(8.0)),
+                                gradient: LinearGradient(
+                                    colors: [
+                                      Palette.buttonColor,
+                                      Palette.nameColor,
+                                    ]
                                 ),
                               ),
-                              /*end of password*/
-
-
-                              /*next button*/
-                              Container(
-                                margin: EdgeInsets.symmetric(vertical: 10),
-                                alignment: Alignment.center,
-                                width: double.infinity,
+                              /*button*/
+                              child: ButtonTheme(
                                 height: 50.0,
-                                /*button colors*/
-                                decoration: BoxDecoration(
-                                  borderRadius: BorderRadius.all(Radius.circular(8.0)),
-                                  gradient: LinearGradient(
-                                      colors: [
-                                        Palette.buttonColor,
-                                        Palette.nameColor,
-                                      ]
-                                  ),
-                                ),
-                                /*button*/
-                                child: ButtonTheme(
-                                  height: 50.0,
-                                  minWidth: 350,
-                                  child: FlatButton(onPressed: (){
-                                    /*go to sign up page*/
-                                    Navigator.pushNamed(context, '/signupBirthday');
-                                  },
-                                    child: Text('Next',
-                                      style: TextStyle(
-                                        color: Palette.backgroundColor,
-                                        fontWeight: FontWeight.bold,
-                                        fontSize: 18,
-                                      ),
+                                minWidth: 350,
+                                child: FlatButton(onPressed: (){
+                                  /*go to sign up page*/
+                                  Navigator.pushNamed(context, '/signupBirthday');
+                                },
+                                  child: const Text('Next',
+                                    style: TextStyle(
+                                      color: Palette.backgroundColor,
+                                      fontWeight: FontWeight.bold,
+                                      fontSize: 18,
                                     ),
                                   ),
                                 ),
                               ),
-                              /*end of next button */
+                            ),
+                            /*end of next button */
 
-                            ],
-                          ),]
-                      ),
+                          ],
+                        ),]
                     ),
-                    /*end form*/
-                  ],
-                ),
+                  ),
+                  /*end form*/
+                ],
               ),
             ),
+          ),
 
-            /*log out?*/
-            Column(
-              mainAxisAlignment: MainAxisAlignment.end,
-              children: [
-                Divider(
-                  height: 5,
-                ),
-                Container(
-                  padding: EdgeInsets.symmetric(vertical: 25, horizontal: 25),
-                  child: Row(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children:[
-                        //Already have an account?
-                        Text(
-                          "Already have an account? ",
+          /*log out?*/
+          Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            children: [
+              const Divider(
+                height: 5,
+              ),
+              Container(
+                padding: const EdgeInsets.symmetric(vertical: 25, horizontal: 25),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children:[
+                      //Already have an account?
+                      const Text(
+                        "Already have an account? ",
+                        style: TextStyle(
+                          color: Palette.grey,
+                        ),
+                      ),
+                      //Log in
+                      InkWell(
+                        child: const Text(
+                          'Log In.',
                           style: TextStyle(
-                            color: Palette.grey,
+                            color: Palette.link,
+                            fontWeight: FontWeight.bold,
                           ),
                         ),
-                        //Log in
-                        InkWell(
-                          child: new Text(
-                            'Log In.',
-                            style: TextStyle(
-                              color: Palette.link,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          onTap: () => Navigator.pushNamed(context, '/login'),
-                        ),
+                        onTap: () => Navigator.pushNamed(context, '/login'),
+                      ),
 
 
-                      ]
-                  ),
-                )
-              ],
-            )
-            //ean of login?
+                    ]
+                ),
+              )
+            ],
+          )
+          //ean of login?
 
-          ],
-        ),
+        ],
       ),
     );
   }
