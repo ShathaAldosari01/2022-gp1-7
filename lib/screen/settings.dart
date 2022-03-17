924a977abf749b60f6662dc2f1fb5432516618dd
Ghala Alghofaili <ghalakg.x@gmail.com>
2022-03-17 18:57:18 +0300

    delete user account

--- lib/screen/settings.dart
+++ lib/screen/settings.dart
@@ -9,6 +9,7 @@ import 'package:focused_menu/modals.dart';
 import 'package:gp1_7_2022/config/palette.dart';
 import 'package:gp1_7_2022/Widgets/follow_button.dart';
 import 'package:rflutter_alert/rflutter_alert.dart';
+import 'package:gp1_7_2022/screen/auth/signup_login.dart';
 
 
 class settings extends StatefulWidget {
@@ -19,19 +20,61 @@ class settings extends StatefulWidget {
 }
 
 class _settingsState extends State<settings> {
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         backgroundColor: Palette.backgroundColor,
 
         appBar: AppBar(
+
+          leading: IconButton(
+            icon: const Icon(
+                Icons.arrow_back, color: Colors.black
+            ),
+            onPressed: () => Navigator.pushNamed(context, '/'),
+          ),
           elevation: 0,
           backgroundColor: Palette.backgroundColor,
         ),
 
+      body:
 
 
+    Container(
+    margin: EdgeInsets.symmetric(horizontal: 30.0,vertical: 10),
+    alignment: Alignment.center,
+    width: double.infinity,
+    height: 50.0,
+    /*button colors*/
+    decoration: BoxDecoration(
+    borderRadius: BorderRadius.all(Radius.circular(8.0)),
+    gradient: LinearGradient(
+    colors: [
+    Palette.buttonColor,
+    Palette.nameColor,
+    ]
+    ),
+    ),
+    /*button*/
+    child: ButtonTheme(
+    height: 50.0,
+    minWidth: 350,
+    child: FlatButton(onPressed: (){
 
+     FirebaseAuth.instance.currentUser!.delete();
+     Navigator.pushNamed(context, '/');
+    },
+    child: Text('delete account',
+    style: TextStyle(
+    color: Palette.backgroundColor,
+    fontWeight: FontWeight.bold,
+    fontSize: 18,
+    ),
+    ),
+    ),
+    ),
+    ),
 
     );
   }
