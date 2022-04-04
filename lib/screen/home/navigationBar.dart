181f4f7ed564cc9b71acc0614f14350f16a25529
YaraAlrubaiya <439201251@student.ksu.edu.sa>
2022-04-04 22:09:54 +0300

    Added navigation bar

---
+++ lib/screen/home/navigationBar.dart
@@ -0,0 +1,71 @@
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:gp1_7_2022/config/palette.dart';
+import 'package:gp1_7_2022/screen/home/UserProfile/Profile_Page.dart';
+import 'package:gp1_7_2022/screen/home/home_page.dart';
+import 'package:gp1_7_2022/screen/home/lists.dart';
+import 'package:gp1_7_2022/screen/home/notification_page.dart';
+import 'package:gp1_7_2022/screen/home/search_page.dart';
+import 'package:gp1_7_2022/screen/home/settings_page.dart';
+
+
+class navigationBar extends StatefulWidget {
+  @override
+  _navigationBarState createState() => _navigationBarState();
+}
+
+class _navigationBarState extends State<navigationBar> {
+  int index = 0;
+
+  final screens = [
+    HomePage(),
+    SearchPage(),
+    AddPostPage(),
+    lists(),
+    NotificationPage(),
+    Profile_page(uid: null,),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    final items = <Widget>[
+      Icon(Icons.home, size: 30),
+      Icon(Icons.search, size: 30),
+      Icon(Icons.add, size: 30),
+      Icon(Icons.list, size: 30),
+      Icon(Icons.notifications_none, size: 30),
+      Icon(Icons.person, size: 30),
+    ];
+
+    return Scaffold(
+      extendBody: true,
+      backgroundColor: Palette.backgroundColor,
+      appBar: AppBar(
+        backgroundColor: Palette.backgroundColor,
+        foregroundColor: Palette.textColor,
+        elevation: 0,//no shadow
+        automaticallyImplyLeading: false,//no arrow
+      ),
+
+      body: screens[index],
+      bottomNavigationBar: Theme(
+        data: Theme.of(context).copyWith(
+          iconTheme: IconThemeData(color: Colors.white),
+        ),
+
+        child: CurvedNavigationBar(
+          color: Palette.buttonColor,
+          // buttonBackgroundColor: Palette.link,
+          backgroundColor: Colors.transparent,
+          height: 60,
+          animationCurve: Curves.easeInOut,
+          animationDuration: Duration(milliseconds: 300),
+          index: index,
+          items: items,
+          onTap: (index) => setState(() => this.index = index),
+        ),
+      ),
+    );
+  }
+}
